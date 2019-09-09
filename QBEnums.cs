0ef0af01063a0eefd30f552d93bd12b07f3db4f9
GTravesty <travis@gamertravesty.com>
2019-09-09 15:26:29 -0400

    Adds conversion method for XElement to Nullable Enum.

--- QBEnums.cs
+++ QBEnums.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace QBSDK_Helper
 {
@@ -11,5 +12,17 @@ namespace QBSDK_Helper
         public enum PreferredDeliveryMethod { None, Email, Fax }
 
         public enum ListType { Account, BillingRate, Class, Currency, Customer, CustomerMsg, CustomerType, DateDrivenTerms, Employee, InventorySite, ItemDiscount, ItemFixedAsset, ItemGroup, ItemInventory, ItemInventoryAssembly, ItemNonInventory, ItemOtherCharge, ItemPayment, ItemSalesTax, ItemSalesTaxGroup, ItemService, ItemSubtotal, JobType, OtherName, PaymentMethod, PayrollItemNonWage, PayrollItemWage, PriceLevel, SalesRep, SalesTaxCode, ShipMethod, StandardTerms, ToDo, UnitOfMeasureSet, Vehicle, Vendor, VendorType, WorkersCompCode }
+
+
+        public static Nullable<T> Parse<T>(this XElement xElement) where T : struct, Enum
+        {
+            if(xElement == null)
+            {
+                return null;
+            }
+            T result = Activator.CreateInstance<T>();
+            Enum.TryParse<T>((string)xElement.Element(typeof(T).Name), out result);
+            return result;
+        }
     }
 }
